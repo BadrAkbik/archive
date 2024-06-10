f47f5993671980298f25fec4b82b96d1d31ada05
BadrAk <badrakbik1@gmail.com>
2024-06-10 14:35:38 +0300

    .

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class FileDownloadController extends Controller
@@ -15,10 +14,9 @@ class FileDownloadController extends Controller
         $fileRecord = File::findOrFail($fileId);
         // Ensure the file exists
         if (!Storage::disk('public')->exists($fileRecord->path)) {
-            Log::error('File does not exist: ' . $fileRecord->path);
             abort(404);
         }
-        Log::info('Downloading file: ' . $fileRecord->path);
+        
         // Return the file as a response
         return Storage::disk('public')->download($fileRecord->path);
     }
